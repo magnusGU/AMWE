7e3025d7f0b142cad1f72d8ecea1fd39837759f1
Magnus Andersson <magnus.andersson@i3tex.com>
2020-09-08 16:39:55 +0200

    Change back class parameters to private for listings, add getters and setters

--- app/src/main/java/com/example/amwe/model/Listings.java
+++ app/src/main/java/com/example/amwe/model/Listings.java
@@ -5,22 +5,22 @@ import android.media.Image;
 import java.util.List;
 
 public class Listings {
-    public int id;
-    public String title;
-    public String edition;
-    public String author;
-    public int isbn;
-    public String description;
-    public Image bookImage;
-    public double price;
 
-    private User seller;
+    private int id;
+    private String title;
+    private String edition;
+    private String author;
+    private int isbn;
+    private String description;
+    private Image bookImage;
+    private double price;
+    private String seller; //Temporary string instead of User
 
     public Listings() {
     }
 
     public Listings(int id, String title, String edition, String author, int isbn,
-                    String description, Image bookImage, double price, User seller) {
+                    String description, Image bookImage, double price, String seller) {
 
         this.id = id;
         this.title = title;
@@ -47,4 +47,77 @@ public class Listings {
                 ", seller=" + seller +
                 '}';
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public void setEdition(String edition) {
+        this.edition = edition;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Image getBookImage() {
+        return bookImage;
+    }
+
+    public void setBookImage(Image bookImage) {
+        this.bookImage = bookImage;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getSeller() {
+        return seller;
+    }
+
+    public void setSeller(String seller) {
+        this.seller = seller;
+    }
+
 }
