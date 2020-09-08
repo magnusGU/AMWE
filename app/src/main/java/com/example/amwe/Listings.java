3d038d2ede4e7fc4d205453d11292eac4e5f5de0
eliajo <eliascarlsward@gmail.com>
2020-09-08 11:34:44 +0200

    added two classes to model, User and Listings

---
+++ app/src/main/java/com/example/amwe/Listings.java
@@ -0,0 +1,15 @@
+package com.example.amwe;
+
+import android.media.Image;
+
+public class Listings {
+    private int id;
+    private String title;
+    private String edition;
+    private String author;
+    private int isbn;
+    private String description;
+    private Image bookImage;
+    private double price;
+    private User seller;
+}
---
+++ app/src/main/java/com/example/amwe/User.java
@@ -0,0 +1,4 @@
+package com.example.amwe;
+
+public class User {
+}
