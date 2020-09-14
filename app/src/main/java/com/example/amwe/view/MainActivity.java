14d92c9e45c8c9bc0a54bde6f6d1ac7b1e8e4d83
William Hugo <william.hugo@hotmail.se>
2020-09-14 10:34:58 +0200

    Made my branch not crash program with latest master merge

--- app/src/main/java/com/example/amwe/view/MainActivity.java
+++ app/src/main/java/com/example/amwe/view/MainActivity.java
@@ -33,21 +33,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        SearchView searchView = findViewById(R.id.action_search);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String s) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String s) {
-                System.out.println("TEXTEN HAR ÄNDRATS HÄR *********************************************************************************************************************");
-                listingAdapter.getFilter().filter(s);
-                return false;
-            }
-        });
+        //A SearchView might need to be created too
         BottomNavigationView bottomNavigation = findViewById(R.id.bottomNavigationView);
         bottomNavigation.setOnNavigationItemSelectedListener(navListner);
         getSupportFragmentManager().beginTransaction().replace(R.id.pages, new searchPage()).commit();
@@ -98,7 +84,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        System.out.println("HEJ");
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.search_function, menu);
         MenuItem searchItem = menu.findItem(R.id.action_search);
