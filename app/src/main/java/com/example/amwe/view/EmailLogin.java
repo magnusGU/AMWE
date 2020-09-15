0fb3b64c2a2c06b8f377e5b908d8c67fbfa8bf41
Ali Alladin <ali.alladin20@gmail.com>
2020-09-15 11:17:51 +0200

    Created a login page, no functionality yet.

---
+++ app/src/main/java/com/example/amwe/view/EmailLogin.java
@@ -0,0 +1,26 @@
+package com.example.amwe.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.amwe.R;
+
+public class EmailLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_login);
+        Button loginButton = findViewById(R.id.log_in_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(EmailLogin.this, MainActivity.class));
+            }
+        });
+    }
+}
