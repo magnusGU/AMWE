3518306ae0047da5a9c5a1eff7cc52606e0c6dc0
Ali Alladin <ali.alladin20@gmail.com>
2020-09-23 10:39:24 +0200

    Created the AddListing activity.

--- app/src/main/java/com/example/amwe/model/Database.java
+++ app/src/main/java/com/example/amwe/model/Database.java
@@ -1,5 +1,9 @@
 package com.example.amwe.model;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
@@ -25,6 +29,25 @@ public class Database {
     public void addUser(String uid, String name){
         User user = new User(name);
         database.getReference().child("users").child(uid).setValue(user);
+    }
+
+    public String getName(String uid){
+        final String[] name = new String[1];
+        database.getReference().child("users").child(uid).addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        name[0] = snapshot.getValue(User.class).getName();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                }
+                );
+        System.out.println(name[0]);
+        return name[0];
     }
     
 }
---
+++ app/src/main/java/com/example/amwe/view/AddListing.java
@@ -0,0 +1,16 @@
+package com.example.amwe.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.amwe.R;
+
+public class AddListing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_listing);
+    }
+}
