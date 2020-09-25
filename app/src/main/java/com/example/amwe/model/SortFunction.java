20dbd0733f999e636e64b63ee1e022cd4cb73914
William Hugo <william.hugo@hotmail.se>
2020-09-25 12:40:44 +0200

    Found a way easier way to sort, also made tests

--- app/src/main/java/com/example/amwe/model/SortFunction.java
+++ app/src/main/java/com/example/amwe/model/SortFunction.java
@@ -15,63 +15,25 @@ public class SortFunction {
         this.originalList = new ArrayList<Listing>(list);
     }
 
-    private ArrayList<Listing> sortPrice() {
-        double[] prices = new double[list.size()];
-        ArrayList<Listing> updatedOrder = new ArrayList();
-
-        for (int i = 0; i < prices.length; i++) { //get all prices
-            prices[i] = list.get(i).getPrice();
-        }
-
-        for (int i = 0; i < prices.length-1; i++) { //bubble-sorting, sorts
-            for (int j = 0; j < prices.length-i-1; j++) {
-                if (prices[j] > prices[j+1]) {
-                    double temp = prices[j];
-                    prices[j] = prices[j+1];
-                    prices[j+1] = temp;
-                }
-            }
-        }
-
-        for (double d:prices) {
-            for (Listing l:list) {
-                if(l.getPrice() == d) {
-                    updatedOrder.add(l);
-                    break;
-                }
-            }
-        }
-
-        return updatedOrder;
-    }
-
-    private ArrayList<Listing> sortAlphabetically() {
-        ArrayList<String> titles = new ArrayList<>();
-        ArrayList<Listing> updatedOrder = new ArrayList<>();
-
-        for (Listing l:list) { //get all titles
-            titles.add(l.getTitle());
-        }
-
-        Collections.sort(titles, new Comparator<String>() { //sorts list of titles alphabetically
+    public void sortPrice() {
+        Collections.sort(list, new Comparator<Listing>() {
             @Override
-            public int compare(String s1, String s2) {
-                return s1.compareToIgnoreCase(s2);
+            public int compare(Listing l1, Listing l2) {
+                return Double.compare(l1.getPrice(),l2.getPrice());
             }
         });
-
-        for (String s: titles) { //sorts listings to be ordered like the list of titles
-            for (Listing l:list) {
-                if (l.getTitle().equals(s)) {
-                    updatedOrder.add(l);
-                    break;
-                }
-            }
-        }
-
-        return updatedOrder;
     }
 
-    private ArrayList<Listing> getOriginalList() {return originalList;}
+    public void sortAlphabetically() {
+        Collections.sort(list, new Comparator<Listing>() {
+            @Override
+            public int compare(Listing l1, Listing l2) {
+                return l1.getTitle().compareToIgnoreCase(l2.getTitle());
+            }
+        });
+    }
 
+    public ArrayList<Listing> getOriginalList() {
+        return originalList;
+    }
 }
